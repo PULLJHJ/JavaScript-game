7ee16cca9b4f206e353007ca86b6401266e3e50d
songkungsug <songkungsug@gmail.com>
2023-09-08 16:19:27 +0900

    filter수정중

--- src/main/java/com/game/team1/Team1Application.java
+++ src/main/java/com/game/team1/Team1Application.java
@@ -3,9 +3,11 @@ package com.game.team1;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan
+@ComponentScan
 public class Team1Application {
 
 	public static void main(String[] args) {

50edb88c00871e85ca783bddd49451269033808e
songkungsuk <songkungsug@gmail.com>
2023-09-09 21:30:39 +0900

    filter complete

--- src/main/java/com/game/team1/filter/Team5Fillter.java
+++ src/main/java/com/game/team1/filter/Team5Fillter.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.FilterChain;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -19,27 +20,31 @@ import com.game.team1.vo.UserInfoVO;
 
 import lombok.extern.slf4j.Slf4j;
 
-@WebFilter(value = { "/" })
+@WebFilter(value = { "/index2", "/tmpl/*" })
 @Slf4j
 public class Team5Fillter extends GenericFilterBean {
 
-    private List<String> execludeUrls = new ArrayList<>();
-    {
-        execludeUrls.add("/index");
-    }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         if (request instanceof HttpServletRequest req && response instanceof HttpServletResponse res) {
-            HttpSession session = req.getSession();
+            List<String> reqUri = new ArrayList<String>();
+            reqUri.add("/index");
+            //----------------------------------------------------------------
+        	HttpSession session = req.getSession();
             UserInfoVO user = (UserInfoVO) session.getAttribute("user");
             log.info("user=>{}", user);
-            String uri = req.getRequestURI();
-            if (!execludeUrls.contains(uri)) {
+            //----------------------------------------------------------------
+            String uri = req.getRequestURI();  //사용자가 입력한 uri 에서
+            log.info("uri=>{}",uri);
+            if (!reqUri.contains(uri)) { // 내가 설정한 Url가 없으면! 있으면 그냥 필터실행
                 if (user == null) {
                     res.sendRedirect("/index");
-                    return;
+                    
+                }
+                else {
+                	return;
                 }
             }
 
@@ -48,3 +53,4 @@ public class Team5Fillter extends GenericFilterBean {
 
     }
 }
+

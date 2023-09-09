9429929cb317eab03bcb207e6c28b2392a328011
seungho <fbtmdgh4517@naver.com>
2023-09-10 00:21:05 +0900

    webfilter 작동 되게 수정

--- src/main/java/com/game/team1/filter/Team5Fillter.java
+++ src/main/java/com/game/team1/filter/Team5Fillter.java
@@ -8,24 +8,27 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
 import com.game.team1.vo.UserInfoVO;
 
 import lombok.extern.slf4j.Slf4j;
 
-@WebFilter(value = { "/" })
+@Component
+// @WebFilter(urlPatterns = "/tmpl/user-info/list")
 @Slf4j
 public class Team5Fillter extends GenericFilterBean {
 
     private List<String> execludeUrls = new ArrayList<>();
     {
-        execludeUrls.add("/index");
+        execludeUrls.add("/tmpl/user-info/join");
+        execludeUrls.add("/tmpl/user-info/login");
+        execludeUrls.add("/");
     }
 
     @Override
@@ -38,7 +41,7 @@ public class Team5Fillter extends GenericFilterBean {
             String uri = req.getRequestURI();
             if (!execludeUrls.contains(uri)) {
                 if (user == null) {
-                    res.sendRedirect("/index");
+                    res.sendRedirect("/");
                     return;
                 }
             }

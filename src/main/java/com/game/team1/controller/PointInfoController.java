a0b86e286b42143544180b127fc9be2c73f20198
류승호 <fbtmdgh4517@naver.com>
2023-09-13 16:03:24 +0900

    insertPointInfo 파라미터 옆에 @RequestBody 추가

--- src/main/java/com/game/team1/controller/PointInfoController.java
+++ src/main/java/com/game/team1/controller/PointInfoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.game.team1.service.PointInfoService;
@@ -21,7 +22,7 @@ public class PointInfoController {
     }
 
     @PostMapping("/point-infos")
-    public int insertPointInfo(PointInfoVO pointInfo) {
+    public int insertPointInfo(@RequestBody PointInfoVO pointInfo) {
         return piServce.insertPointInfo(pointInfo);
     }
 }

f57b6dbff330186699efd33d25880c2079e0574b
PULLJHJ <ju9612yong@gmail.com>
2023-09-14 11:18:37 +0900

    df

--- src/main/java/com/game/team1/controller/PointInfoController.java
+++ src/main/java/com/game/team1/controller/PointInfoController.java
@@ -2,18 +2,14 @@ package com.game.team1.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.game.team1.service.PointInfoService;
 import com.game.team1.vo.PointInfoVO;
-import com.game.team1.vo.UserInfoVO;
 
 @RestController
 public class PointInfoController {
@@ -28,12 +24,5 @@ public class PointInfoController {
     @PostMapping("/point-infos")
     public int insertPointInfo(@RequestBody PointInfoVO pointInfo) {
         return piServce.insertPointInfo(pointInfo);
-    }
-
-    @GetMapping("/point-infos/max")
-    public PointInfoVO getMaxPointInfo(PointInfoVO point, HttpSession session){
-        UserInfoVO user = (UserInfoVO) session.getAttribute("user");
-        point.setUiNum(user.getUiNum());
-        return piServce.selectMaxPoint(point);
     }
 }

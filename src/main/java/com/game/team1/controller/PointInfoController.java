ff02d96a2c78f60c57eeebae1f064d0e600c8b09
songkungsug <songkungsug@gmail.com>
2023-09-15 14:46:19 +0900

    dtd

--- src/main/java/com/game/team1/controller/PointInfoController.java
+++ src/main/java/com/game/team1/controller/PointInfoController.java
@@ -4,10 +4,8 @@ import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
-import org.mariadb.jdbc.type.Point;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
